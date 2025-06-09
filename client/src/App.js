e50a195b7ff6c1ecaf0389a1ab4ff06c085bcd98
muteeurrehman28 <bsai23005@itu.edu.pk>
2025-06-09 22:07:44 +0500

    Add root App component

---
+++ client/src/App.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import theme from './theme';
+
+// Layout Components
+import Navbar from './components/layout/Navbar';
+
+// Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Profile from './pages/Profile';
+import PostJob from './pages/PostJob';
+import JobDetails from './pages/JobDetails';
+import Messages from './pages/Messages';
+
+// Auth Components
+import PrivateRoute from './components/routing/PrivateRoute';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/post-job"
+              element={
+                <PrivateRoute>
+                  <PostJob />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/messages"
+              element={
+                <PrivateRoute>
+                  <Messages />
+                </PrivateRoute>
+              }
+            />
+            <Route path="/jobs/:id" element={<JobDetails />} />
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </Provider>
+  );
+};
+
+export default App; 
